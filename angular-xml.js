b605d89e0354aef065b9735e4f484c86228bdd1c
Duncan Smith <dunksmith@gmail.com>
2016-02-11 15:44:25 +0000

    Better XML detection, remove X2JS dependency, use jQuery.parseXML and custom JS instead

--- angular-xml.js
+++ angular-xml.js
@@ -1,22 +1,17 @@
-if (!X2JS) {
-  throw new Error("You're required to include the X2JS library to use the xml module.");
-}
-
-(function(ng, X2JS) {
+(function(ng) {
 
   function responseIsXml(response) {
     var contentType = response.headers('content-type');
-    if (contentType) {
-      return contentType.search(/\Wxml/i) > -1;
-    } else {
-      return false;
-    }
+    return (
+              (contentType && contentType.search(/\Wxml/i) > -1)
+              || (angular.isString(response.data) && /^<\?xml/i.test(response.data))
+           );
   }
 
-  function xmlHttpInterceptorFactory($q, x2js) {
+  function xmlHttpInterceptorFactory($q) {
     function responseHandler(response) {
       if (response && responseIsXml(response)) {
-        response.data = x2js.xml_str2json(response.data);
+        response.data = xmlToJson(jQuery.parseXML(response.data));
         return response;
       } else {
         return $q.when(response);
@@ -24,7 +19,7 @@ if (!X2JS) {
     }
     function responseErrorHandler(response) {
       if (response && responseIsXml(response)) {
-        response.data = x2js.xml_str2json(response.data);
+        response.data = xmlToJson(jQuery.parseXML(response.data));
       }
       return $q.reject(response);
     }
@@ -35,23 +30,67 @@ if (!X2JS) {
   }
 
   function configProvider($provide) {
-    $provide.factory('xmlHttpInterceptor', ['$q', 'x2js', xmlHttpInterceptorFactory]);
+    $provide.factory('xmlHttpInterceptor', ['$q', xmlHttpInterceptorFactory]);
   }
 
-  function X2JSProvider() {
-    this.config = {};
-    this.$get = ['X2JS', function (X2JS) {
-      return new X2JS(this.config);
-    }];
-  }
+  // See https://davidwalsh.name/convert-xml-json / http://plnkr.co/edit/YLJSNv?p=info
+  function xmlToJson(xml) {
+      
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) { // element
+      // do attributes
+      if (xml.attributes.length > 0) {
+        //obj["attributes"] = {};
+        for (var j = 0; j < xml.attributes.length; j++) {
+          var attribute = xml.attributes.item(j);
+          //obj["attributes"][attribute.nodeName] = attribute.nodeValue;
+          obj['@' + attribute.nodeName] = attribute.nodeValue;
+        }
+      }
+    } else if (xml.nodeType == 3) { // text
+      obj = xml.nodeValue.trim(); // add trim here
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+      for (var i = 0; i < xml.childNodes.length; i++) {
+        var item = xml.childNodes.item(i);
+        var nodeName = item.nodeName;
+        //  console.debug('child',nodeName,item)
+        if (typeof(obj[nodeName]) == "undefined") {
+          var tmp = xmlToJson(item);
+          if (tmp !== "") // if not empty string
+            obj[nodeName] = tmp;
+        } else {
+          if (typeof(obj[nodeName].push) == "undefined") {
+            var old = obj[nodeName];
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          var tmp = xmlToJson(item);
+          if (tmp !== "") // if not empty string
+            obj[nodeName].push(tmp);
+        }
+      }
+    }
+    if (!Array.isArray(obj) && typeof obj == 'object') {
+      var keys = Object.keys(obj);
+      if (keys.length == 1 && keys[0] == '#text') return obj['#text'];
+      if (keys.length === 0) return null;
+    }
+    return obj;
+  };
 
   if (ng) {
     ng
       .module('xml', [])
       .config(['$provide', configProvider])
-      .provider('x2js', X2JSProvider)
-      .value('X2JS', X2JS);
+      .factory('xmlToJson', function() {
+         return { convert: xmlToJson };
+      });
   }
 
-}(angular, X2JS));
+}(angular));
 
